c379d95653d134e8a8091cec82ddb9555427cba1
Carson Ellis <crsnell25@gmail.com>
2021-12-21 19:21:12 -0500

    First Commit

---
+++ Q2A.py
@@ -0,0 +1,45 @@
+# Question 2A from the Google Foobar challenge
+#
+# The problem is:
+#
+#       Given squares a and b, return the smallest number of moves to get from a to b on a chess board
+#
+#       The chess board squares are 0-63
+#
+#       Additional requirement: MUST BE FAST
+
+import timeit
+import numpy
+
+def gen_moves(memo, dest, num_moves):
+    for i, n in enumerate(memo[:]):
+        if not n:
+            continue
+        for a, b in [(2, 1), (-2, 1), (2, -1), (-2, -1), (1, 2), (-1, 2), (1, -2), (-1, -2)]:
+            x = (i%8) + a 
+            y = i + 8 * b
+            if 0 > x or x > 7 or 0 > y  or y > 63:
+                continue
+            i_ = (i + a) + (8 * b)
+            if i_ == dest:
+                return num_moves
+            memo[i_] = True
+
+    return gen_moves(memo, dest, num_moves + 1)
+   
+
+def solution(src, dest):
+    moves = [[2, 1], [-2, 1], [2, -1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]]
+    memo = [False] * 64
+    memo[src] = True
+    n = gen_moves(memo, dest, 1)
+    return n
+
+
+def main():
+    print(solution(0, 1))
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Q2B.py
@@ -0,0 +1,44 @@
+# Question 2B from the Google Foobar challenge
+
+# The problem is: 
+#        '>' = Person walking right
+#        '<' = Person walking left
+#        '-' = Space
+#        
+#        When a person walking right crosses a person walking left they must salute eachother
+#
+#        Count the number of salutes that occur if each person walks in their direction and return this number
+#
+#        For example, '<<>><' will return 4
+
+def solution(s):
+    i, j = 0, len(s) - 1
+    while i <= j and s[i] != '>':
+        i += 1
+    found = True
+    salutes = 0
+    while found and i < j:
+        iter = i + 1
+        found = False
+        while iter <= j:
+            if (not found) and s[iter] == '>':
+                found = True
+                i = iter
+            if s[iter] == '<':
+                salutes += 2
+            iter += 1
+    return salutes
+
+
+
+if __name__ == "__main__":
+    s = "--->-><-><-->-"
+    s2 = ">>>>>>>>>>>--------><"
+    s3 = "<----<---<"
+    s4 = ">---->>>----<"
+    s5 = "<<>><"
+    print(solution(s))
+    print(solution(s2))
+    print(solution(s3))
+    print(solution(s4))
+    print(solution(s5))
